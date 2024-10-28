fda42d5a750b75b3c80118764f605d96a64c6806
RobertWesner <robert@wesner.io>
2024-10-28 11:55:01 +0100

    #13: Bump Version after rebase

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Play All
 // @description     Adds the Play-All-Button to the videos, shorts, and live sections of a YouTube-Channel
-// @version         20241028-0-beta
+// @version         20241028-1-beta
 // @author          Robert Wesner (https://robert.wesner.io)
 // @license         MIT
 // @namespace       http://robert.wesner.io/
