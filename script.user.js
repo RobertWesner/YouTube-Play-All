RobertWesner/YouTube-Play-All

commit 421aead1c4ec9aa26a9ebc142f02582788f80f67

Update to work with new mobile view and point to #39 on oldest

script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Play All
 // @description     Adds the Play-All-Button to the videos, shorts, and live sections of a YouTube-Channel
-// @version         20250620-0
+// @version         20250929-0
 // @author          Robert Wesner (https://robert.wesner.io)
 // @license         MIT
 // @namespace       http://robert.wesner.io/
@@ -93,6 +93,11 @@
 
         .ytpa-random-btn > .ytpa-btn-section:hover, .ytpa-random-popover > *:hover {
             background-color: #6192ee;
+        }
+        
+        .ytpa-play-all-btn.ytpa-unsupported {
+            background-color: #828282;
+            color: white;
         }
         
         .ytpa-random-popover {
@@ -244,7 +249,7 @@
         }
         
         /* hide when sorting by oldest */
-        ytm-feed-filter-chip-bar-renderer > div :nth-child(3).selected ~ .ytpa-btn, ytd-feed-filter-chip-bar-renderer iron-selector#chips :nth-child(3).iron-selected ~ .ytpa-btn {
+        ytm-feed-filter-chip-bar-renderer > div :nth-child(3).selected ~ .ytpa-btn:not(.ytpa-unsupported), ytd-feed-filter-chip-bar-renderer iron-selector#chips :nth-child(3).iron-selected ~ .ytpa-btn:not(.ytpa-unsupported) {
             display: none;
         }
     </style>`);
@@ -287,7 +292,7 @@
     const apply = () => {
         let parent = location.host === 'm.youtube.com'
             // mobile view
-            ? document.querySelector('ytm-feed-filter-chip-bar-renderer > div')
+            ? document.querySelector('ytm-feed-filter-chip-bar-renderer .chip-bar-contents, ytm-feed-filter-chip-bar-renderer > div')
             // desktop view
             : document.querySelector('ytd-feed-filter-chip-bar-renderer iron-selector#chips');
 
@@ -316,10 +321,15 @@
                 'beforeend',
                 `<a class="ytpa-btn ytpa-play-all-btn" href="/playlist?list=${popularPlaylist}${id}&playnext=1">Play Popular</a>`
             );
-        } else {
+        } else if (parent.querySelector(':nth-child(1).selected, :nth-child(1).iron-selected')) {
             parent.insertAdjacentHTML(
                 'beforeend',
                 `<a class="ytpa-btn ytpa-play-all-btn" href="/playlist?list=${allPlaylist}${id}&playnext=1">Play All</a>`
+            );
+        } else {
+            parent.insertAdjacentHTML(
+                'beforeend',
+                `<a class="ytpa-btn ytpa-play-all-btn ytpa-unsupported" href="https://github.com/RobertWesner/YouTube-Play-All/issues/39" target="_blank">Not Playlist Found</a>`
             );
         }
 
@@ -368,13 +378,30 @@
         }
     };
 
-    const observer = new MutationObserver(apply);
+    const observer = new MutationObserver(() => {
+        // [20250929-0] removeButton first and then apply, not addButton, since we don't need the pathname validation, and we want mobile to also use it
+        removeButton();
+        apply();
+    });
+
     const addButton = async () => {
         observer.disconnect();
 
         if (!(window.location.pathname.endsWith('/videos') || window.location.pathname.endsWith('/shorts') || window.location.pathname.endsWith('/streams'))) {
             return;
         }
+
+        // Regenerate button if switched between Latest and Popular
+        const element = document.querySelector('ytd-rich-grid-renderer, ytm-feed-filter-chip-bar-renderer .iron-selected, ytm-feed-filter-chip-bar-renderer .chip-bar-contents .selected');
+        if (!element) {
+            return;
+        }
+
+        observer.observe(element, {
+            attributes: true,
+            childList: false,
+            subtree: false
+        });
 
         // This check is necessary for the mobile Interval
         if (document.querySelector('.ytpa-play-all-btn')) {
@@ -387,18 +414,6 @@
 
         // Initially generate button
         apply();
-
-        // Regenerate button if switched between Latest and Popular
-        const element = document.querySelector('ytd-rich-grid-renderer');
-        if (!element) {
-            return;
-        }
-
-        observer.observe(element, {
-            attributes: true,
-            childList: false,
-            subtree: false
-        });
     };
 
     // Removing the button prevents it from still existing when switching between "Videos", "Shorts", and "Live"

commit 48345be991ea47a6042870fc63bf4c3afb9ac002

#40 Fix premature setting of the id caused by commit 421aead

script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Play All
 // @description     Adds the Play-All-Button to the videos, shorts, and live sections of a YouTube-Channel
-// @version         20250929-0
+// @version         20251002-0
 // @author          Robert Wesner (https://robert.wesner.io)
 // @license         MIT
 // @namespace       http://robert.wesner.io/
@@ -288,8 +288,13 @@
         }
     };
 
-    let id;
+    let id = '';
     const apply = () => {
+        if (id === '') {
+            // do not apply prematurely, caused by mutation observer
+            return;
+        }
+
         let parent = location.host === 'm.youtube.com'
             // mobile view
             ? document.querySelector('ytm-feed-filter-chip-bar-renderer .chip-bar-contents, ytm-feed-filter-chip-bar-renderer > div')
@@ -393,15 +398,13 @@
 
         // Regenerate button if switched between Latest and Popular
         const element = document.querySelector('ytd-rich-grid-renderer, ytm-feed-filter-chip-bar-renderer .iron-selected, ytm-feed-filter-chip-bar-renderer .chip-bar-contents .selected');
-        if (!element) {
-            return;
+        if (element) {
+            observer.observe(element, {
+                attributes: true,
+                childList: false,
+                subtree: false
+            });
         }
-
-        observer.observe(element, {
-            attributes: true,
-            childList: false,
-            subtree: false
-        });
 
         // This check is necessary for the mobile Interval
         if (document.querySelector('.ytpa-play-all-btn')) {