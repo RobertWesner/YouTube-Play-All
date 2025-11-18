b712e797a2eae9b4066400a493b2e0cdd1e21539
RobertWesner <robert@wesner.io>
2025-11-18 13:40:29 +0100

    Added security focused eslint plugins.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 import js from './linting/node_modules/@eslint/js/src/index.js';
 import globals from './linting/node_modules/globals/index.js';
 import { defineConfig } from './linting/node_modules/eslint/lib/config-api.js';
+import security from './linting/node_modules/eslint-plugin-security/index.js';
+import noUnsanitized from './linting/node_modules/eslint-plugin-no-unsanitized/index.js';
 
 export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs}'],
-        plugins: { js },
+        plugins: {
+            js,
+            security,
+            'no-unsanitized': noUnsanitized,
+        },
         extends: ['js/recommended'],
         languageOptions: {
             globals: {
@@ -27,7 +33,15 @@ export default defineConfig([
             'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
             'prefer-const': 'warn',
             eqeqeq: ['error', 'always'],
-            'no-var': 'error'
-        }
+            'no-var': 'error',
+            'security/detect-eval-with-expression': 'error',
+            'security/detect-non-literal-regexp': 'warn',
+            'security/detect-non-literal-require': 'off',
+            'security/detect-object-injection': 'off',
+            'security/detect-possible-timing-attacks': 'off',
+            'security/detect-pseudoRandomBytes': 'off',
+            'no-unsanitized/method': 'error',
+            'no-unsanitized/property': 'error',
+        },
     },
 ]);
